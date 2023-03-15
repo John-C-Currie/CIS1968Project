5371853c076d4a8b5342944c03a640ad54e10404
zantuaw09 <werty7532@gmail.com>
2023-03-15 10:28:12 -0400

    Basic FileReader code

--- FileReader.java
+++ FileReader.java
@@ -1,4 +1,32 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+
 public class FileReader 
 {
-    
+    public void lines(String filename)
+    {
+        try
+        {
+            Scanner scan = new Scanner(new File(filename));
+            while (scan.hasNextLine())
+            {
+                scan.useDelimiter(",");
+                String line = scan.next();
+                System.out.println(line);
+            }
+            scan.close();
+        }
+
+        catch (FileNotFoundException e)
+        {
+            System.out.println("ERROR: File Not Found");
+            e.printStackTrace();
+        }
+    }
+    public static void main(String args[])
+    {
+        FileReader read = new FileReader();
+        read.lines("tree2021.csv");
+    }
 }
