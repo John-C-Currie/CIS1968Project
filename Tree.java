964be900448ec201971fe6592f835dfe16da760a
John Currie <john.connor.currie@gmail.com>
2023-03-17 09:45:36 -0400

    Created get/set methods

--- Tree.java
+++ Tree.java
@@ -1,15 +1,54 @@
 public class Tree 
 {
-    double xcor, ycor;
-    String treeType;
-    double DBH;
- 
-    public Tree(double x, double y, String type, double DBH)
+    private double xcor, ycor;
+    private String treeType;
+    private double DBH;
+
+    public double getXcor()
+    {
+        return this.xcor;
+    }
+    
+    public double getYcor()
+    {
+        return this.ycor;
+    }
+
+    public String getTreeType()
+    {
+        return this.treeType;
+    }
+
+    public double getDBH()
+    {
+        return this.DBH;
+    }
+
+    public void setXcor(double x)
     {
         this.xcor = x;
+    }
+
+    public void setYcor(double y)
+    {
         this.ycor = y;
+    }
+
+    public void setTreeType(String type)
+    {
         this.treeType = type;
+    }
+
+    public void setDBH(double DBH)
+    {
         this.DBH = DBH;
+    }
+    public Tree(double x, double y, String type, double DBH)
+    {
+        setXcor(x);
+        setYcor(y);
+        setTreeType(type);
+        setDBH(DBH);
     }
 
     public Tree(double x, double y)
