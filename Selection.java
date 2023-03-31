9a2139728098b2e27d642116322dd884fb6e195b
John Currie <john.connor.currie@gmail.com>
2023-03-31 11:41:34 -0400

    Changed the selection class to be static so that new objects are not being created everytime selection is called.

--- Selection.java
+++ Selection.java
@@ -2,20 +2,15 @@ import java.util.ArrayList;
 
 public class Selection  // class to find all nearby trees at a specific point on the map
 {
-    private double xCoor, yCoor;
+    private static double xCoor, yCoor;
 
-    private double WPP = Conversion.convertWidthPerPixel("PhilaCnty1854.png");
-    private double HPP = Conversion.convertHeightPerPixel("PhilaCnty1854.png");
+    private static double WPP = Conversion.convertWidthPerPixel("PhilaCnty1854.png");
+    private static double HPP = Conversion.convertHeightPerPixel("PhilaCnty1854.png");
     
-    public Selection(int x, int y)
+    public static void setCordinates(int x, int y)
     {
-        this.xCoor = Conversion.widthToLong(x, WPP);
-        this.yCoor = Conversion.heightToLat(y, HPP);
-    }
-
-    public String toString()
-    {
-        return "X: " + this.xCoor + "\t Y: " + this.yCoor;
+        xCoor = Conversion.widthToLong(x, WPP);
+        yCoor = Conversion.heightToLat(y, HPP);
     }
 
     public static void main(String args[])
